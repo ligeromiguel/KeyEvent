30c43369caa2a4bbc0db687e974bf1811e6ebdd1
Miguel Ligero <Miguel Ligero@Equipo7>
2020-10-19 09:40:21 +0200

    Finished work!

---
+++ src/lambdakeyevent/LambdaKeyEvent.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambdakeyevent;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Miguel Ligero
+ */
+public class LambdaKeyEvent extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        //Creación del Texto, sus coordenadas iniciales y valor
+        Text tx = new Text (20,20,"A");
+        
+        //Evento SetOnKeyPressed (Lambda)
+        tx.setOnKeyPressed((KeyEvent e) -> {  
+            switch(e.getCode())
+            {
+                case DOWN:
+                    tx.setLayoutY(tx.getLayoutY()+10);
+                    break;
+                case UP:
+                    tx.setLayoutY(tx.getLayoutY()-10);
+                    break;
+                case RIGHT:
+                    tx.setLayoutX(tx.getLayoutX()+10);
+                    break;
+                case LEFT:
+                    tx.setLayoutX(tx.getLayoutX()-10);
+                    break;
+                default:
+                    tx.setText(tx.getText());
+                    break;
+           
+        }
+                  
+    });
+        
+        
+        Pane root = new Pane();
+  
+        tx.setFocusTraversable(true);
+        root.getChildren().add(tx);
+        Scene scene = new Scene(root, 220, 40);
+        
+        primaryStage.setTitle("KeyEvent");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
